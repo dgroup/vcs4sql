91467f3bda5815215f0a66cd6e52ad3cf22934b7
Yurii Dubinka <yurii.dubinka@gmail.com>
2020-01-16 22:24:52 +0530

    Fix 0pdd issue with "too many leading spaces"
    Reading .rubocop.yml...
    Reading todo.yml...
    ERROR: todo.yml; puzzle at line #16; Too many leading spaces at 24:2; make sure all lines that include the puzzle body start at position #3.
    If you can't understand the cause of this issue or you don't know how to fix it, please submit a GitHub issue, we will try to help you: https://github.com/yegor256/pdd/issues. This tool is still in its beta version and we will appreciate your feedback. Here is where you can find more documentation: https://github.com/yegor256/pdd/blob/master/README.md.
    Exit code is 1
    
    /app/objects/git_repo.rb:66:in `rescue in block in xml'
    /app/objects/git_repo.rb:63:in `block in xml'
    /app/vendor/ruby-2.6.0/lib/ruby/2.6.0/tempfile.rb:295:in `open'
    /app/objects/git_repo.rb:62:in `xml'
    /app/objects/puzzles.rb:36:in `deploy'
    /app/objects/job.rb:38:in `proceed'
    /app/objects/job_starred.rb:33:in `proceed'
    /app/objects/job_recorded.rb:32:in `proceed'
    /app/objects/job_emailed.rb:35:in `proceed'
    /app/objects/job_commiterrors.rb:36:in `proceed'
    /app/objects/job_detached.rb:48:in `exclusive'
    /app/objects/job_detached.rb:36:in `block in proceed'
    /app/objects/job_detached.rb:36:in `fork'
    /app/objects/job_detached.rb:36:in `proceed'
    /app/0pdd.rb:358:in `block in '

--- test/vcs4sql/sqlite/migration_test.rb
+++ test/vcs4sql/sqlite/migration_test.rb
@@ -19,7 +19,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-#
 
 require_relative "../../sqlite_test"
 require_relative "../../../lib/vcs4sql/sqlite/migration"
